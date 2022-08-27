297b8ecc3af3feb0b5501e11571c5b1bf676953b
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-08-27 19:58:53 +0530

    created screens initialized firebase

---
+++ config/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getAnalytics } from "firebase/analytics"
+import Constants from "expo-constants"
+
+const firebaseConfig = {
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.authDomain,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId,
+    measurementId: Constants.manifest.extra.measurementId
+}
+
+initializeApp(firebaseConfig)
+export const auth = getAuth()
+export const db = getFirestore()
+export const analytics = getAnalytics()
---
+++ screens/Chat.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Chat extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Chat</Text>
+            </View>
+        )
+    }
+}
+
+export default Chat
